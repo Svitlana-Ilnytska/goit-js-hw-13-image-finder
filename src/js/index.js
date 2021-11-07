0d6cf07f0f2d1de58e333dee3ccffc73ffe270cf
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-11-07 22:34:32 +0200

    added animation

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,38 @@ import onFetchError from './error';
 import infScroll from './inf-scroll';
 import * as _ from 'lodash';
 
+const loading = document.querySelector('.loading');
+
 const refs = getRefs();
 const newsApiService = new NewsApiService();
 
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  console.log({ scrollTop, scrollHeight, clientHeight });
+  if (clientHeight + scrollTop >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(getNewsApiService, 1000);
+}
+function getNewsApiService() {
+  // evt.preventDefault();
+  const searchQuery = refs.input.value;
+
+  newsApiService.incrementPage();
+  newsApiService
+    .fetchImages(searchQuery)
+    .then(appendImagesMarkup)
+    .catch(onFetchError)
+    .finally(() => searchQuery === '');
+}
+
 // refs.searchForm.addEventListener('input', onInputChange);
-refs.searchForm.addEventListener("input", _.debounce(onInputChange, 1000));
+refs.searchForm.addEventListener('input', _.debounce(onInputChange, 1000));
 
 function onInputChange(evt) {
   evt.preventDefault();
@@ -23,6 +50,7 @@ function onInputChange(evt) {
   if (searchQuery === '') {
     return (refs.photoList.innerHTML = '');
   }
+
   newsApiService.resetPage();
   clearArticlesContainer();
   newsApiService
@@ -36,6 +64,7 @@ function appendImagesMarkup(images) {
   refs.photoList.innerHTML = '';
   if (images.hits.length >= 1) {
     refs.photoList.insertAdjacentHTML('beforeend', ImageCardTpl(images.hits));
+    loading.classList.remove('show');
   } else {
     onFetchError();
   }
--- src/js/inf-scroll.js
+++ src/js/inf-scroll.js
@@ -1,27 +1,27 @@
-import InfiniteScroll from 'infinite-scroll';
+// import InfiniteScroll from 'infinite-scroll';
 
-const API_KEY = '24180904-cdf25eb395cc9b94381638218';
-const BASE_URL = 'https://pixabay.com';
+// const API_KEY = '24180904-cdf25eb395cc9b94381638218';
+// const BASE_URL = 'https://pixabay.com';
 
-const infScroll = new InfiniteScroll('.gallery', {
-  responseType: 'text',
-  loadCount: 1,
-  history: false,
-  // responseBody: 'json',
-  status: '.scroll-status',
-  visibleStyle: { transform: 'translateY(0)', opacity: 1 },
-  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
-  path: function () {
-    let pageNumber = this.pageNumber += 1;
-    return `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=cat&page=${pageNumber}&per_page=12&key=${API_KEY}`;
-  },
-});
+// const infScroll = new InfiniteScroll('.gallery', {
+//   responseType: 'text',
+//   loadCount: 1,
+//   history: false,
+//   // responseBody: 'json',
+//   status: '.scroll-status',
+//   visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+//   hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
+//   path: function () {
+//     let pageNumber = ( this.loadCount + 1 ) * 12;
+//     return `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=cat&page=${pageNumber}&per_page=12&key=${API_KEY}`;
+//   },
+// });
 
-infScroll.loadNextPage();
+// infScroll.loadNextPage();
 
-infScroll.on('load', (response, path) => {
-  console.log(JSON.parse(response));
-});
+// infScroll.on('load', (response, path) => {
+//   console.log(JSON.parse(response));
+// });
 
 
   // тут по шаблну сделали строку с тегами
@@ -33,3 +33,60 @@ infScroll.on('load', (response, path) => {
 // const d = document.createElement('div');
 // fragment.innerHTML = markup;
 // console.log(fragment);
+
+// const container = document.getElementById('container');
+// const loading = document.querySelector('.loading');
+
+// getPost();
+// getPost();
+// getPost();
+
+// window.addEventListener('scroll', () => {
+// 	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	
+// 	console.log( { scrollTop, scrollHeight, clientHeight });
+	
+// 	if(clientHeight + scrollTop >= scrollHeight - 5) {
+// 		// show the loading animation
+// 		showLoading();
+// 	}
+// });
+
+// function showLoading() {
+// 	loading.classList.add('show');
+	
+// 	// load more data
+// 	setTimeout(getPost, 1000)
+// }
+
+// async function getPost() {
+// 	const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${getRandomNr()}`);
+// 	const postData = await postResponse.json();
+	
+// 	const userResponse = await fetch('https://randomuser.me/api');
+// 	const userData = await userResponse.json();
+	
+// 	const data = { post: postData, user: userData.results[0] };
+	
+// 	addDataToDOM(data);
+// }
+
+// function getRandomNr() {
+// 	return Math.floor(Math.random() * 100) + 1;
+// }
+
+// function addDataToDOM(data) {
+// 	const postElement = document.createElement('div');
+// 	postElement.classList.add('blog-post');
+// 	postElement.innerHTML = `
+// 		<h2 class="title">${data.post.title}</h2>
+// 		<p class="text">${data.post.body}</p>
+// 		<div class="user-info">
+// 			<img src="${data.user.picture.large}" alt="${data.user.name.first}" />
+// 			<span>${data.user.name.first} ${data.user.name.last}</span>
+// 		</div>
+// 	`;
+// 	container.appendChild(postElement);
+	
+// 	loading.classList.remove('show');
+// }
